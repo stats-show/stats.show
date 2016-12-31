9b6147e0213d17c02e4d3ffe7b14498dc8a99471
Dmitry Zaets <dmitry.zaets@gmail.com>
2016-12-31 10:56:02 +0100

    Move deploy to travis config

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "Setting up GitHub user info"
-git config --global user.name "$USER_NAME"
-git config --global user.email "$USER_EMAIL"
-echo "Setting up ssh key"
-ssh-add id_rsa
 echo "Executing npm run deploy"
 npm run deploy
