8f6da23340f0f9bc345924c66def94d5877f2c7a
Dmitry Zaets <dmitry.zaets@gmail.com>
2016-12-31 11:51:56 +0100

    Add direct url

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,4 @@ eval `ssh-agent -s`
 ssh-add id_rsa
 ssh-add -l
 
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-
-gh-pages -d build -r $SSH_REPO
+gh-pages -d build -r git@github.com:StarRatio/star-ratio.git
