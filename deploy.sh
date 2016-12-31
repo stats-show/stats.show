52cf23e404f855078d45bb5ebffb14de4c770e4d
Dmitry Zaets <dmitry.zaets@gmail.com>
2016-12-31 11:48:21 +0100

    Try git url

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,8 @@ chmod 600 id_rsa
 eval `ssh-agent -s`
 ssh-add id_rsa
 ssh-add -l
-npm run deploy
+
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+gh-pages -d build -r $SSH_REPO
