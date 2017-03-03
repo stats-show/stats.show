7d0754827785b6fa0430fbc5fb5eef71928ffad3
Dmitry Zaets <dmitry.zaets@gmail.com>
2017-03-03 18:57:59 +0100

    Change github url

--- packages/net/src/components/GithubButton/index.js
+++ packages/net/src/components/GithubButton/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import "./index.css";
 
@@ -8,13 +8,14 @@ class GithubButton extends Component {
       <div className="github-button-container github-header-button">
         <a
           className="github-button"
-          href="https://github.com/StarRatio/star-ratio"
+          href="https://github.com/stats-show/stats.show"
           data-icon="octicon-star"
           data-style="mega"
           data-count-href="/StarRatio/star-ratio/stargazers"
           data-count-api="/repos/StarRatio/star-ratio#stargazers_count"
           data-count-aria-label="# stargazers on GitHub"
-          aria-label="Star StarRatio/star-ratio on GitHub">
+          aria-label="Star StarRatio/star-ratio on GitHub"
+        >
           Star
         </a>
       </div>
